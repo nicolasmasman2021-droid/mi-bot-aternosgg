3524dccf97114d2d827211d7dd124ab8916fc51b
nicolasmasman2021-droid <nicolasmasman2021@gmail.com>
2026-07-27 21:03:33 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'lossahurysushijitos.aternos.me',
+        port: 21146,
+        username: 'nicobot :D',
+        version: '1.21.1'
+    });
+
+    bot.on('chat', (username, message) => {
+        if (username === bot.username) return;
+        bot.chat('Sigo activo');
+    });
+
+    bot.on('kick', (reason) => console.log('Expulsado:', reason));
+    bot.on('error', (err) => console.log('Error:', err));
+    bot.on('end', () => {
+        console.log('Bot desconectado, reintentando en 30 segundos...');
+        setTimeout(createBot, 30000);
+    });
+}
+
+createBot();
